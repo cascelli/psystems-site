4e31ca52365445d340683ffc1bce5b60eeaa7a8c
Marcos Antonio Cascelli <marcos.cascelli@gmail.com>
2022-05-13 09:39:05 -0300

    Criado arquivo ".env" para armazenamento de dados da aplicação atraves de constantes para  facilitar personalizacao ao publicar o site em clientes distintos

--- src/sdk/services/UrlLink.service.ts
+++ src/sdk/services/UrlLink.service.ts
@@ -2,6 +2,11 @@ import Service from '../Service';
 import { UrlLink } from '../@types/UrlLink';
 
 class UrlLinkService extends Service {
+  //OBS: Para dados simples, usar variaveis de ambiente no arquivo .env do projeto
+  //     Em seguida, defina uma constante que referencia o valor armazernado na mesma
+  //     EX: REACT_APP_URLS_LINKS = [conteúdo a ser armazenado]
+  //     EX: const APP_URLS_LINKS = process.env.REACT_APP_URLS_LINKS;
+
   static getAllUrlLinks() {
     // todo: substituir a chamada Http.get pela leitura do arquivo json
     //       de dados da pagina assets/dados/urlsLinks.txt
